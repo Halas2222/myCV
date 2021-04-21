d609783ff44ef61a8e2242d95b9ad3dfb5449864
Paweł Hałas <halas2222@gmail.com>
2021-04-21 20:40:08 +0200

    fixed hamburger

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Obsługa zdarzenia pisania na maszynie 
+
 const text = document.querySelector('.description');
 const spnCursor = document.querySelector('.cursor');
 const txt = 'Hej!!! Jestem Paweł. Początkujący Front End Developer. Ogólnie znana teza głosi, iż użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, sprawiającego, że wygląda to „zbyt czytelnie” po polsku. Wielu webmasterów i designerów używa Lorem Ipsum jako domyślnego modelu tekstu i wpisanie w internetowej wyszukiwarce ‘lorem ipsum’ spowoduje znalezienie bardzo wielu stron, które wciąż są w budowie. Wiele wersji tekstu ewoluowało i zmieniało się przez lata, czasem przez przypadek, czasem specjalnie (humorystyczne'  
@@ -15,3 +17,18 @@ const cursorAnimation = () => {
 
 const indexTyping = setInterval(addLetter, 70);
 setInterval(cursorAnimation, 400);
+
+//  Obsługa zdarzenia klika na menu hamburger
+
+const burger = document.querySelector(".hamburger");
+const bars = document.querySelector(".fa-bars");
+const iconx = document.querySelector(".fa-times");
+const nav = document.querySelector("nav");
+
+
+burger.addEventListener("click", function() {
+    bars.classList.toggle("show");
+    iconx.classList.toggle("show");
+    nav.classList.toggle("dissapear");
+}
+)
