f5e9e6b366a56c2542199e0179e5e5a0fd025382
Paweł Hałas <halas2222@gmail.com>
2021-04-25 13:52:00 +0200

    create section skills

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Obsługa zdarzenia pisania na maszynie 
-
 const text = document.querySelector('.description');
 const spnCursor = document.querySelector('.cursor');
 const txt = 'Hej!!! Jestem Paweł. Początkujący Front End Developer. Ogólnie znana teza głosi, iż użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, sprawiającego, że wygląda to „zbyt czytelnie” po polsku. Wielu webmasterów i designerów używa Lorem Ipsum jako domyślnego modelu tekstu i wpisanie w internetowej wyszukiwarce ‘lorem ipsum’ spowoduje znalezienie bardzo wielu stron, które wciąż są w budowie. Wiele wersji tekstu ewoluowało i zmieniało się przez lata, czasem przez przypadek, czasem specjalnie (humorystyczne'  
 
+
+
 let indexText = 0;
-const addLetter = () => {
+const addLetter = function ()  {
 text.textContent += txt[indexText];
 indexText++;
 if(indexText === txt.length) clearInterval(indexTyping);
 }
+
 
 const cursorAnimation = () => {
     spnCursor.classList.toggle('active');
@@ -25,10 +27,12 @@ const bars = document.querySelector(".fa-bars");
 const iconx = document.querySelector(".fa-times");
 const nav = document.querySelector("nav");
 
-
-burger.addEventListener("click", function() {
+const clickBurger = function() {
     bars.classList.toggle("show");
-    iconx.classList.toggle("show");
-    nav.classList.toggle("dissapear");
+        iconx.classList.toggle("show");
+        nav.classList.toggle("dissapear");
+
 }
-)
+
+burger.addEventListener("click", clickBurger);
+setTimeout(clickBurger, 4000);
